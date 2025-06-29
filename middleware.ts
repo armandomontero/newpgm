832546d542e549675020afc2fe89ef223e73a096
ArmandoDev <amonterof@gmail.com>
2025-06-28 22:41:28 -0400

    quitamos api/empresa de public route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs/server'
 
 export default authMiddleware({
-  publicRoutes:["/api/uploadthing", "/api/empresa"]
+  publicRoutes:["/api/uploadthing"]
 })
 
 export const config = {
